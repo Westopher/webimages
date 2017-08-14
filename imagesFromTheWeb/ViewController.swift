9f36dce9a72cdf94c63be0f7662d22968b9c54b1
West Kraemer <kraemerwest@gmail.com>
2017-08-14 13:21:15 -0400

    add image from the web

--- imagesFromTheWeb/ViewController.swift
+++ imagesFromTheWeb/ViewController.swift
@@ -9,10 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var bachImageView: UIImageView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = URL(string: "https://upload.wikimedia.org/wikipedia/commons/6/6a/Johann_Sebastian_Bach.jpg")!
+        
+        let request = NSMutableURLRequest(url: url)
+        
+        let task = URLSession.shared.dataTask(with: request as URLRequest) {
+            
+            data, response, error in
+            
+            if error != nil {
+                
+                print(error)
+                
+            } else {
+                
+                if let data = data {
+                
+                if let bachImage = UIImage(data: data) {
+                
+                    self.bachImageView.image = bachImage
+                    
+                    }
+                }
+                
+            }
+            
+        }
+        
+        task.resume()
+        
     }
 
     override func didReceiveMemoryWarning() {
